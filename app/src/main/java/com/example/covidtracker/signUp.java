8fc5b6df2242eb77f64a58f3b45e637fdcb77adc
oliwur98 <oliver982009@hotmail.com>
2021-09-14 15:02:21 +0200

    hejhej

--- app/src/main/java/com/example/covidtracker/signUp.java
+++ app/src/main/java/com/example/covidtracker/signUp.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class signUp extends AppCompatActivity {
     //register stuff
@@ -37,25 +39,49 @@ public class signUp extends AppCompatActivity {
         CharSequence seq = text.getText().toString();
         return TextUtils.isEmpty(seq);
     }
-    public void SignUpCheck(){
-        if(isEmpty(EditTextFirstname)){
+    boolean noNumber(EditText text) {
+        String str = text.getText().toString();
+
+            for(int i = 0; i < str.length(); i++) {
+                if (!Character.isLetter(str.charAt(i))) return true;
+            }
+            return false;
+    }
+    boolean realEmail(EditText text){
+        CharSequence seq = text.getText().toString();
+        return (!Patterns.EMAIL_ADDRESS.matcher(seq).matches());
+    }
+
+    public void SignUpCheck() {
+
+        if (isEmpty(EditTextFirstname)) {
             EditTextFirstname.setError("Field is required");
+        } else if (noNumber(EditTextFirstname)) {
+            EditTextFirstname.setError("Only letters");
         }
-        if(isEmpty(EditTextLastname)){
+        if (isEmpty(EditTextLastname)) {
             EditTextLastname.setError("Field is required");
+        } else if (noNumber(EditTextLastname)) {
+            EditTextLastname.setError("Only letters");
         }
-        if(isEmpty(EditTextEmail)){
+        if (isEmpty(EditTextEmail)) {
             EditTextEmail.setError("Field is required");
-        }
-        if(isEmpty(EditTextSSN)){
-            EditTextSSN.setError("Field is required");
-        }
-        if(isEmpty(EditTextPassword)){
-            EditTextPassword.setError("Field is required");
-        }
-        if(isEmpty(EditTextPasswordConfirm)){
-            EditTextPasswordConfirm.setError("Field is required");
+        } else if (realEmail(EditTextEmail)) {
+            EditTextEmail.setError("Enter a valid email");
         }
 
+        if (isEmpty(EditTextSSN)) {
+            EditTextSSN.setError("Field is required");
+        }
+        if (isEmpty(EditTextPassword)) {
+            EditTextPassword.setError("Field is required");
+        }
+        if (isEmpty(EditTextPasswordConfirm)) {
+            EditTextPasswordConfirm.setError("Field is required");
+        } else if (EditTextPassword.getText().toString() == EditTextPasswordConfirm.getText().toString()) {
+            EditTextPasswordConfirm.setError("Password doesn't match");
+        }
+
+
     }
-}
+    }
