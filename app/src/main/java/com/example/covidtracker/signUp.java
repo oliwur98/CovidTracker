e9d0f420cc4d8198ff46c1877dc643ecf6d1ea93
oliwur98 <oliver982009@hotmail.com>
2021-09-14 12:56:17 +0200

    hejhej

--- app/src/main/java/com/example/covidtracker/signUp.java
+++ app/src/main/java/com/example/covidtracker/signUp.java
@@ -11,19 +11,41 @@ import android.widget.Toast;
 
 public class signUp extends AppCompatActivity {
     //register stuff
-    EditText firstname;
+    EditText EditTextFirstname, EditTextLastname, EditTextEmail, EditTextSSN, EditTextPassword, EditTextPasswordConfirm;
+    Button BtnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         //register stuff
-        firstname=(EditText) findViewById(R.id.Firstname);
+        EditTextFirstname =(EditText) findViewById(R.id.Firstname);
+        EditTextLastname =(EditText) findViewById(R.id.Lastname);
+        EditTextEmail = (EditText) findViewById(R.id.Email_signup);
+        EditTextSSN = (EditText) findViewById(R.id.SSN_signup);
+        EditTextPassword =(EditText) findViewById(R.id.Password_signup);
+        EditTextPasswordConfirm =(EditText) findViewById(R.id.VerifyPassword_signup);
+        BtnSignUp =(Button) findViewById(R.id.BtnRegister);
+        BtnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Firstname, Lastname, Email, SSN, Password, PasswordConfirm;
+                int finalSSN;
+                Firstname = String.valueOf(EditTextFirstname.getText());
+                Lastname = String.valueOf(EditTextLastname.getText());
+                Email = String.valueOf(EditTextEmail.getText());
+                SSN = String.valueOf(EditTextSSN.getText());
+                finalSSN = Integer.parseInt(SSN);
+                Password = String.valueOf(EditTextPassword.getText());
+                PasswordConfirm = String.valueOf(EditTextPasswordConfirm.getText());
+                Toast.makeText(getApplicationContext(), "missing stuff",Toast.LENGTH_LONG);
+             /*   if(!Firstname.equals("") && !Lastname.equals("") && !Email.equals("") && !SSN.equals("") && !Password.equals("") && !PasswordConfirm.equals("")) {
 
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "missing stuff",Toast.LENGTH_LONG);
+                }*/
+            }
+        });
     }
 
-    public void register(View view){
-        //just a test to see if button works
-        Toast.makeText(this, "hejhej", Toast.LENGTH_SHORT).show();
-
-    }
 }
