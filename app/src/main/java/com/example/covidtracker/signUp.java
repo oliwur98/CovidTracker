aa7aa0aca604685cd89a18ec018ecd159875f7ff
oliwur98 <oliver982009@hotmail.com>
2021-09-22 11:15:49 +0200

    hejhej

--- app/src/main/java/com/example/covidtracker/signUp.java
+++ app/src/main/java/com/example/covidtracker/signUp.java
@@ -1,5 +1,6 @@
 package com.example.covidtracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,13 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class signUp extends AppCompatActivity {
     //register stuff
     private EditText EditTextFirstname, EditTextLastname, EditTextEmail, EditTextSSN, EditTextPassword, EditTextPasswordConfirm;
-    Button BtnSignUp;
+    private Button BtnSignUp;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,22 +35,36 @@ public class signUp extends AppCompatActivity {
         EditTextPassword =(EditText) findViewById(R.id.Password_signup);
         EditTextPasswordConfirm =(EditText) findViewById(R.id.VerifyPassword_signup);
         BtnSignUp =(Button) findViewById(R.id.BtnRegister);
+        auth = FirebaseAuth.getInstance();
         BtnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            String email = EditTextEmail.getText().toString();
+            String password =EditTextPassword.getText().toString();
 
-            // SignUpCheck();
 
             if(SignUpCheck()){
 
-                Toast.makeText(signUp.this, "SignUp successful", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(signUp.this,login.class);
-                startActivity(intent);
-            }
-                else{
-                Toast.makeText(signUp.this, "SignUp failed", Toast.LENGTH_SHORT).show();
-                 }
+                auth.createUserWithEmailAndPassword(email,password )
+                        .addOnCompleteListener(signUp.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(signUp.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(signUp.this, "SignUp failed", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(signUp.this, "SignUp successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(signUp.this,login.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
 
+
+            }
 
             }
         });
@@ -100,6 +119,9 @@ public class signUp extends AppCompatActivity {
 
         if (isEmpty(EditTextPassword)) {
             EditTextPassword.setError("Field is required");
+        }
+        else if(EditTextPassword.getText().toString().length() < 6){
+            EditTextPassword.setError("password must be atleast 6 characters long");
         }
         else counter++;
 
